3c37d8e6ee65261422505b37bff88799b7880627
Alexandre <alexandrenas2003@gmail.com>
2026-09-04 10:21:26 -0300

    projeto inicio

---
+++ src/main/java/com/realtime/chat/ChatApplication.java
@@ -0,0 +1,11 @@
+package com.realtime.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public final class ChatApplication {
+	static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+}
